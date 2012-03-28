e1e039d436a3d030cb08ac7ee0ff85e1c181e654
Rikard Andersson <mail@rikardandersson.se>
2012-03-28 16:42:04 +0200

    Created Android-project: Muffins

---
+++ Muffins/src/itBrainiacs/muffin/MuffinsActivity.java
@@ -0,0 +1,13 @@
+package itBrainiacs.muffin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MuffinsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
