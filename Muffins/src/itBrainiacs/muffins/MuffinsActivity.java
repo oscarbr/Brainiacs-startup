f04d10e4da55922b7c8be2de60bd617bf9d5f92a
Rikard Andersson <mail@rikardandersson.se>
2012-03-29 10:58:02 +0200

    Tjena världen has been added

--- Muffins/src/itBrainiacs/muffins/MuffinsActivity.java
+++ Muffins/src/itBrainiacs/muffins/MuffinsActivity.java
@@ -2,12 +2,15 @@ package itBrainiacs.muffins;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MuffinsActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Tjena världen!");
+        setContentView(tv);
     }
 }
