ad44a0a388090f0eda557f75090ca00c84964c46
Rikard Andersson <mail@rikardandersson.se>
2012-04-16 13:41:06 +0200

    Made the SearchResultActivity to be a ListActivity and filled it with place holder information. Requirement ID: NF1

--- Muffins/src/itBrainiacs/muffins/SearchResultActivity.java
+++ Muffins/src/itBrainiacs/muffins/SearchResultActivity.java
@@ -1,18 +1,45 @@
 package itBrainiacs.muffins;
 
-import android.app.Activity;
+import java.util.ArrayList;
+
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.TextView;
 
-public class SearchResultActivity extends Activity {
+public class SearchResultActivity extends ListActivity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		/* Placeholder textview */
+		/* Place holder list */
+		ArrayList<String> ph = new ArrayList<String>();
+		ph.add("Rikard");
+		ph.add("Emil");
+		ph.add("Filip");
+		ph.add("Oscar");
 		
-		TextView tw = new TextView(this);
-		tw.setText("This is where the search results will show up.");
-		this.setContentView(tw);
+		/* The ArrayAdapter will later be of a type which we ourself create i.e. Sale */
+		this.setListAdapter(new ArrayAdapter<String>(this, R.layout.search_results_list_item, ph)); 
+		
+		/* A pointer to our ListView, used to edit settings for the ListView */
+		ListView lv = this.getListView();
+		
+		/* Filters the list if the user starts typing */
+		lv.setTextFilterEnabled(true);
+		
+		lv.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+			
+		});
 	}
 }
