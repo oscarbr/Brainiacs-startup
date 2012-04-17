5ae15f3806a3f8c62b5e1668835d89d11b0145b5
Rikard Andersson <mail@rikardandersson.se>
2012-04-17 14:56:25 +0200

    Made variables with references to the views specified in the layout-XML.

--- Muffins/src/itBrainiacs/muffins/AddBookActivity.java
+++ Muffins/src/itBrainiacs/muffins/AddBookActivity.java
@@ -2,12 +2,24 @@ package itBrainiacs.muffins;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddBookActivity extends Activity {
+	
+	
+	/* Variables to be act as hooks to views specified in the layout-XML */
+	private EditText authorET;
+	private EditText titleET;
+	private Button addButton;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.add_book_layout);
+		
+		/* Getting references to the objects (views) specified in the layout-XML */
+		authorET = (EditText) findViewById(R.id.addBookAuthorET);
+		titleET = (EditText) findViewById(R.id.addBookTitleET);
+		addButton = (Button) findViewById(R.id.addBookAddButton);
 	}
 }
