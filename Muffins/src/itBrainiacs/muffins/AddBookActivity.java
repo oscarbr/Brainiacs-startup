f8596a0f2cfce03ef51166e0c43ed54d09e73339
Filip Askviken <filip@askviken.se>
2012-04-18 16:30:03 +0200

    RID: 1,2 m: rebased application

--- Muffins/src/itBrainiacs/muffins/AddBookActivity.java
+++ Muffins/src/itBrainiacs/muffins/AddBookActivity.java
@@ -1,7 +1,10 @@
 package itBrainiacs.muffins;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,9 +16,19 @@ public class AddBookActivity extends Activity implements OnClickListener {
 	/* Variables to be act as hooks to views specified in the layout-XML */
 	private EditText authorET;
 	private EditText titleET;
+	private EditText ISBNET;
+	private EditText versionET;
+	private EditText courseET;
+	private EditText priceET;
+	private EditText commentET;
+	private Button takePictureButton;
 	private Button addButton;
-
-
+	
+	private final static int CAMERA_CATCHING_CODE = 100; //The code for activity request camera
+	private final static int MEDIA_TYPE_IMAGE = 1;
+	private Uri imageUri;
+	
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.add_book_layout);
@@ -23,6 +36,13 @@ public class AddBookActivity extends Activity implements OnClickListener {
 		/* Getting references to the objects (views) specified in the layout-XML */
 		authorET = (EditText) findViewById(R.id.addBookAuthorET);
 		titleET = (EditText) findViewById(R.id.addBookTitleET);
+		ISBNET = (EditText) findViewById(R.id.addBookISBNET);
+		versionET = (EditText) findViewById(R.id.addBookVersionET);
+		courseET = (EditText) findViewById(R.id.addBookCourseET);
+		priceET = (EditText) findViewById(R.id.addBookPriceET);
+		commentET = (EditText) findViewById(R.id.addBookCommentET);
+		
+		takePictureButton = (Button) findViewById(R.id.addBookTakePictureButton);
 		addButton = (Button) findViewById(R.id.addBookAddButton);
 		
 		/* Setting the 'add' button to execute onClick() when pressed */
@@ -35,13 +55,66 @@ public class AddBookActivity extends Activity implements OnClickListener {
 		/* Get the Strings from the field edited by the user */
 		String author = authorET.getText().toString();
 		String title = titleET.getText().toString();
+		String isbn = ISBNET.getText().toString();
+		String version = versionET.getText().toString();
+		String course = courseET.getText().toString();
+		String price = priceET.getText().toString();
+		String comment = commentET.getText().toString();
 		
 		/* Place holder Toast to notify that the listener works */
-		Toast.makeText(getApplicationContext(), author + ", " + title + " has been added", Toast.LENGTH_LONG).show();
+		Toast.makeText(getApplicationContext(), "A book with"
+				+ "\nAuthor: " + author 
+				+ "\nTitle: " + title 
+				+ "\nISBN: " + isbn
+				+ "\nVersion: " + version
+				+ "\nCourse: " + course
+				+ "\nPrice: " + price
+				+ "\nComment: " + comment
+				+ "\nhas been added", Toast.LENGTH_LONG).show();
+	}
+	
+	/* Method called when the 'Take Picture' button has been pressed and the user wants to add a photo of their book */
+	private void takePicture() {
+		
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		//http://developer.android.com/guide/topics/media/camera.html#intents
+		//imageUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+	}
+	
+	/* On receiving the picture taken by the camera app. */
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		/* The Result comes from a camera activity */
+		if (requestCode == CAMERA_CATCHING_CODE) {
+	        
+			/* Image has been captured and saved to a path specified in the intent */
+			if (resultCode == RESULT_OK) {
+				
+				data.getData();
+			
+				/* Image has not been captured due to user cancelling process */
+	        } else if (resultCode == RESULT_CANCELED) {
+	        	
+	        	Toast.makeText(getApplicationContext(), "Oh, snap, you cancelled, why did you do that?", Toast.LENGTH_SHORT).show();
+	        
+	        /* Something has gone terribly wrong and the application failed to capture image. */
+	        } else {
+	        	
+	        	Toast.makeText(getApplicationContext(), "Ehh, something went wrong and no image was captured", Toast.LENGTH_SHORT);
+	        }
+		}
 	}
 	
 	/* Method from the interface OnClickListener. Called when the any button that has this class as a listener is pressed*/
 	public void onClick(View view) {
-		this.addBook();
+		if (view.getId() == R.id.addBookTakePictureButton) {
+			
+			takePicture();
+			
+		} else if (view.getId() == R.id.addBookAddButton) {
+			
+			addBook();
+		}
 	}
 }
--- Muffins/src/itBrainiacs/muffins/SearchActivity.java
+++ Muffins/src/itBrainiacs/muffins/SearchActivity.java
@@ -14,8 +14,11 @@ public class SearchActivity extends Activity implements OnClickListener {
 	
 	/* Variables to act as references to views specified in the layout-XML */
 	private Button searchButton;
+	private EditText isbnET;
 	private EditText authorET;
 	private EditText titleET;
+	private EditText versionET;
+	private EditText courseET;
 	
 	/** Called when the activity is initially created */
 
@@ -24,8 +27,11 @@ public class SearchActivity extends Activity implements OnClickListener {
         this.setContentView(R.layout.search_layout);
         
 		/* Getting references to the objects (views) specified in the layout-XML */
+        isbnET = (EditText) findViewById(R.id.searchISBNET);
         authorET = (EditText) findViewById(R.id.searchAuthorET);
         titleET = (EditText) findViewById(R.id.searchTitleET);
+        versionET = (EditText) findViewById(R.id.searchVersionET);
+        courseET = (EditText) findViewById(R.id.searchCourseET);
         searchButton = (Button) findViewById(R.id.searchSearchButton);
         
 		/* Setting the 'add' button to execute onClick() when pressed */
@@ -38,9 +44,17 @@ public class SearchActivity extends Activity implements OnClickListener {
 		/* Fetching the text filled in by the user in the fields */
 		String author = authorET.getText().toString();
 		String title = titleET.getText().toString();
+		String isbn = isbnET.getText().toString();
+		String version = versionET.getText().toString();
+		String course = courseET.getText().toString();
 		
 		/* Place holder notifying that the button has been pressed and that these parametres have been found */
-		Toast.makeText(getApplicationContext(), author + ", " + title + " has been searched", Toast.LENGTH_LONG).show();
+		Toast.makeText(getApplicationContext(), "Author: " + author 
+				+ "\nTitle:" + title 
+				+ "\nISBN:" + isbn 
+				+ "\nVersion: " + version
+				+ "\nCourse: " + course 
+				+ "\nhas been searched", Toast.LENGTH_LONG).show();
 	}
 	
 	/* Method called when the searchButton is pressed */
