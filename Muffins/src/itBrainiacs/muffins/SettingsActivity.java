0fc603009472cd23ecc6cf724468af5b1b68ac5a
Oscar Brodefors <oscar.brodefors@gmail.com>
2012-04-23 13:51:46 +0200

    RID: 4.1 m: Added basic layout to the settings-tab

--- Muffins/src/itBrainiacs/muffins/SettingsActivity.java
+++ Muffins/src/itBrainiacs/muffins/SettingsActivity.java
@@ -2,16 +2,21 @@ package itBrainiacs.muffins;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
 	
+	private EditText emailET;
+	private EditText phoneET;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.settings_layout);
 		
-		TextView tw = new TextView(this);
-		tw.setText("This is where settings of the app will go. Here you will enter your personal information");
-		this.setContentView(tw);
+		/* Getting references to the objects (views) specified in settings_layout.xml */
+		emailET = (EditText) findViewById(R.id.settingsEmailET);
+		phoneET = (EditText) findViewById(R.id.settingsPhoneET);
 	}
 	
 }
