364ec4bfdf309e3f0753c64dddcf2383598d26f2
Oscar Brodefors <oscar.brodefors@gmail.com>
2012-04-23 11:05:54 +0200

    Adding restraints to input while adding a new book

--- Muffins/src/itBrainiacs/muffins/AddBookActivity.java
+++ Muffins/src/itBrainiacs/muffins/AddBookActivity.java
@@ -1,6 +1,7 @@
 package itBrainiacs.muffins;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
 public class AddBookActivity extends Activity implements OnClickListener {
 	
@@ -53,20 +55,30 @@ public class AddBookActivity extends Activity implements OnClickListener {
 	
 	/* Method called when the 'Add' button has been pressed and the user wants to add a book to the database */
 	private void addBook() {
+		boolean validInput;
 		
-		
-		/* Get the Strings from the field edited by the user */
+		/* Get the Strings from the field edited by the user */		
 		String author = authorET.getText().toString();
 		String title = titleET.getText().toString();
-		String isbn = ISBNET.getText().toString();
+		String isbnString = ISBNET.getText().toString();
 		String version = versionET.getText().toString();
 		String pubYear = pubYearET.getText().toString();
 		String course = courseET.getText().toString();
-		String price = priceET.getText().toString();
+		String priceString = priceET.getText().toString();
 		String comment = commentET.getText().toString();
+		Integer isbn = 0;
+		Integer price = 0;
 		
-		/* Place holder Toast to notify that the listener works */
-		Toast.makeText(getApplicationContext(), "A book with"
+		if (isbnString.length() != 0)
+			isbn = Integer.parseInt(isbnString);
+		if (priceString.length() != 0)
+			price = Integer.parseInt(priceString);
+				
+		validInput = checkInput(author, title, isbn, version, course, price, comment);
+		if (validInput) {
+		
+			/* Place holder Toast to notify that the listener works */
+			Toast.makeText(getApplicationContext(), "A book with"
 				+ "\nAuthor: " + author 
 				+ "\nTitle: " + title 
 				+ "\nISBN: " + isbn
@@ -76,7 +88,45 @@ public class AddBookActivity extends Activity implements OnClickListener {
 				+ "\nPrice: " + price
 				+ "\nComment: " + comment
 				+ "\nhas been added", Toast.LENGTH_LONG).show();
+		}
 	}
+	
+	private boolean checkInput(String author, String title, Integer isbn, String version, 
+							String course, Integer price, String comment) {
+		String badInput = "Please correct the following input before proceeding:\n\n";
+		boolean validInput = true;
+		
+		if (author != null && author.length() < 10) {
+			badInput = badInput + "--Author name must be greater than 10 characters.\n";
+			validInput = false;
+		}
+		if (title != null && title.length() < 10) {
+			badInput = badInput + "--Title must be greater than 10 characters.\n";
+		validInput = false;
+		}
+		if ((String.valueOf(isbn).length() != 10) && (String.valueOf(isbn).length() != 13)) {
+			badInput = badInput + "--The ISBN-number must be 10 or 13 digits long.\n";
+		validInput = false;
+		}
+		if (price < 1 || price > 10000) {
+			badInput = badInput + "--Please specify a price within the range of 0-10000 SEK.\n";
+		validInput = false;
+		}
+		
+		if (!validInput) {
+			AlertDialog inputWarningDialog = new AlertDialog.Builder(this).create();
+			inputWarningDialog.setTitle("Invalid input!");
+			inputWarningDialog.setMessage(badInput);
+			inputWarningDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			   public void onClick(DialogInterface dialog, int which) {
+			      // here you can add functions
+			   }
+			});
+			inputWarningDialog.show();
+		}
+			
+		return validInput;
+		}
 	
 	/* Method called when the 'Take Picture' button has been pressed and the user wants to add a photo of their book */
 	private void takePicture() {
