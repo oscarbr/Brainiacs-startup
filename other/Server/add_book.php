7ad08ca0334b40ab35b5fa7e25584dda5f494d07
Rikard Andersson <mail@rikardandersson.se>
2012-04-25 15:53:25 +0200

    RID: 1.1, 1.2 m:Server side scripts now find books and add books.

--- other/Server/add_book.php
+++ other/Server/add_book.php
@@ -1,29 +1,28 @@
 <?php
 	//Get the information from the request (POST)
-	
 	$jsonInput = $_POST['jsondata'];
 	$decoded = json_decode($jsonInput,true);
 	
+	$username = "muffin_master";
+	$password = "***";
+	$server = "localhost";
+		
 	//Connect to the database
-	$connection = mysql_connect(localhost, ***, ***); //might change this to be localhost
-	
-	if (!$connection) {
-		die("Could not connect to db: " . mysql_error()); 	
-	}
-	
+	$connection = mysql_connect($server, $username, $password) or die ("Could not connect to db: " . mysql_error());
+
 	//Select the database to be used
-	mysql_select_db("muffin_books", $connection) or die(mysql_error());
+	mysql_select_db("muffin_books", $connection) or die (mysql_error());
 	
-	$sql = "INSERT INTO Books (author, title, testPerson) VALUES ( '" . $decoded['author'] . "', '" .  $decoded['title'] . "', 'Rikard Andersson')"
-	
-	//Make the query to the database from the POST-form
-	if(!mysql_query($sql, $connection)) {
-		die('Error: ' . mysql_error());
+	//Construct the insert-query
+	$sql = "INSERT INTO Books (author, title, testPerson) VALUES ('" . $decoded['author'] . "', '" . $decoded['title'] . "', 'Rikard Andersson')";
+		
+	//Make the query to the database with the connection
+	if(!mysql_query($sql, $connection)){
+		die("Could not add book to db" . mysql_error());
 	}
 	
-	//Lets the application know that a row has been added
-	echo "1 row added";
+	echo "Book added to database";
 		
 	//Disconnect database
-	mysql_close($connection);	
+	mysql_close($connection);
 ?>
--- other/Server/search_books.php
+++ other/Server/search_books.php
@@ -2,26 +2,23 @@
 	//Get the information from the request (POST)
 	$jsonInput = $_POST['jsondata'];
 	$decoded = json_decode($jsonInput,true);
-
+	
+	$username = "muffin_master";
+	$password = "***";
+	$server = "localhost";
+		
 	//Connect to the database
-	$connection = mysql_connect(localhost:3306, ***, ***) or die ("Could not connect to db: " . mysql_error());
-
+	$connection = mysql_connect($server, $username, $password) or die ("Could not connect to db: " . mysql_error());
 
 	//Select the database to be used
-	mysql_select_db("muffin_books", $connection) or die(mysql_error());
-	
+	mysql_select_db("muffin_books", $connection) or die (mysql_error());
 	
 	//Construct the query
-	$sql = "SELECT * FROM Books WHERE author=" . $decoded['author'] . " AND title=" . $decoded['title'] . ";
+	$sql = "SELECT * FROM Books WHERE author='" . $decoded['author'] . "' AND title='" . $decoded['title'] . "'";
 	
 	//Make the query to the database with the connection
-	$query_results = mysql_query($sql, $connection); 
-	
-	//If no results were returned
-	if (!$query_results) {
-		die("Could not connect to db: ", mysql_error());
-	}
-	
+	$query_results = mysql_query($sql, $connection);
+		
 	//Get the array from the results
 	$info = mysql_fetch_array($query_results, MYSQL_ASSOC);
 	
@@ -30,6 +27,5 @@
 	
 	//Disconnect database
 	mysql_close($connection);
-
 ?>
 
