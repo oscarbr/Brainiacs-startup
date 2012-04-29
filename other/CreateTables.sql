7f3e2b929a61ffa78f3a6b216b48c9cca0f60392
Rikard Andersson <mail@rikardandersson.se>
2012-04-29 13:42:34 +0200

    RID: 1.1, 1.2 m:Made the CreateTable-file compatible with the new server and database.

--- other/CreateTables.sql
+++ other/CreateTables.sql
@@ -1,21 +1,19 @@
-delimiter $$
-
-CREATE TABLE `Entries` (
-  `id` int(11) NOT NULL,
-  `isbn` int(13) DEFAULT NULL,
-  `author` varchar(45) NOT NULL,
-  `title` varchar(45) NOT NULL,
-  `edition` varchar(45) DEFAULT NULL,
-  `year` year(4) DEFAULT NULL,
-  `course` varchar(45) DEFAULT NULL,
-  `period` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  `sold` tinyint(1) DEFAULT NULL,
-  `price` varchar(45) NOT NULL,
-  `expires` date NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `phone` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1$$
+CREATE TABLE Books (
+  saleID int(11) NOT NULL,
+  isbn int(13) DEFAULT NULL,
+  author varchar(100) NOT NULL,
+  title varchar(100) NOT NULL,
+  edition varchar(45) DEFAULT NULL,
+  publYear year(4) DEFAULT NULL,
+  course char(6) DEFAULT NULL,
+  period varchar(45) DEFAULT NULL,
+  bookType varchar(45) DEFAULT NULL,
+  sold int(1) DEFAULT NULL,
+  price varchar(45) NOT NULL,
+  expires date NOT NULL,
+  email varchar(45) NOT NULL,
+  phone varchar(45) NOT NULL,
+  password varchar(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id_UNIQUE (id)
+)
