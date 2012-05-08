79cf3795f1ff40f6b92982a23c7bfbd52a5dd29e
Rikard Andersson <mail@rikardandersson.se>
2012-05-08 10:33:33 +0200

    RID: 1.1, 1.2 m:Added a Factory that creates a List of DataBooks from a JSONObject

---
+++ Muffins/src/itBrainiacs/muffins/DataBookFactory.java
@@ -0,0 +1,39 @@
+package itBrainiacs.muffins;
+
+import java.util.LinkedList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.text.format.DateFormat;
+
+public class DataBookFactory {
+
+	/** 
+	 * Static method returning a List of all the books contained in a JSONObject
+	 * 
+	 *@param JSONObject that contains one or several books
+	 *@return True if the add was successfull
+	 */
+	public static LinkedList<DataBook> jsonToDataBooks(JSONObject jsonObject) {
+		
+		LinkedList<DataBook> dataBooks = new LinkedList<DataBook>();
+		
+		DataBook book = new DataBook();
+		
+		try {
+			book.setTitle(jsonObject.get("title").toString());
+			book.setAuthor(jsonObject.get("author").toString());
+			book.setPrice(jsonObject.get("price").toString());
+			book.setExpires(jsonObject.get("expires").toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		dataBooks.add(book);
+		
+		
+		return dataBooks;
+	}
+}
