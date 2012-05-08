e29695de580b22281f63f0c1c723e9a28f40e814
Filip Askviken <filip@askviken.se>
2012-05-08 10:05:48 +0200

    RID:1.1 & 2.1 m:Added Data class attributes for ad from database to DataBook

--- Muffins/src/itBrainiacs/muffins/DataBook.java
+++ Muffins/src/itBrainiacs/muffins/DataBook.java
@@ -1,5 +1,9 @@
 package itBrainiacs.muffins;
 
+import java.util.Date;
+
+import org.json.JSONObject;
+
 public class DataBook {
 
 	private String title;
@@ -7,6 +11,123 @@ public class DataBook {
 	private String publisher;
 	private String date;
 	private String language;
+	private String isbn;
+	private String edition;
+	private String publYear;
+	private String course;
+	private String period;
+	private String booktype;
+	private Boolean sold;
+	private Integer price;
+	private Date expires;
+	private String email;
+	private String phone;
+	private String password;
+	private String saleId;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public String getPublYear() {
+		return publYear;
+	}
+
+	public void setPublYear(String publYear) {
+		this.publYear = publYear;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getBooktype() {
+		return booktype;
+	}
+
+	public void setBooktype(String booktype) {
+		this.booktype = booktype;
+	}
+
+	public Boolean getSold() {
+		return sold;
+	}
+
+	public void setSold(Boolean sold) {
+		this.sold = sold;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Date getExpires() {
+		return expires;
+	}
+
+	public void setExpires(Date expires) {
+		this.expires = expires;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSaleId() {
+		return saleId;
+	}
+
+	public void setSaleId(String saleId) {
+		this.saleId = saleId;
+	}
 
 	public String getTitle() {
 		return title;
