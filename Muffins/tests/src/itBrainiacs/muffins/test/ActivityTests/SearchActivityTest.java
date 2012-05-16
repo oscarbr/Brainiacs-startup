7e6eb36fef1d4e967b7430ebf477d24241828fa6
Rikard Andersson <mail@rikardandersson.se>
2012-05-16 12:31:11 +0200

    OPEN - task 16: Tests missing for some methods http://github.com/oscarbr/Brainiacs-startup/issues/issue/16 m:Added two simple tests to SearchActivityTest

--- Muffins/tests/src/itBrainiacs/muffins/test/ActivityTests/SearchActivityTest.java
+++ Muffins/tests/src/itBrainiacs/muffins/test/ActivityTests/SearchActivityTest.java
@@ -5,12 +5,27 @@ import itBrainiacs.muffins.SearchActivity;
 
 public class SearchActivityTest extends ActivityInstrumentationTestCase2<SearchActivity> {
 	
+	SearchActivity searchActivity;
+	
+	
 	public SearchActivityTest() {
 		super(SearcActivity.class);
 	}
 	
 	protected setUp() throws Exception {
 		super.setUp();
+		
+		setActivityInitialTouchMode(false);
+				
+		searchActivity = getActivity();
+		
 	}
-
+	
+	public testPreConditions() {
+		assertTrue(searchActivity != null);
+	}
+	
+	public void testOnClick() {
+		searchActivity.onClick(findViewById(itBrainiacs.muffins.R.id.searchSearchButton));
+	}
 }
