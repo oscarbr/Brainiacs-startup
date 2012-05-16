391afad23cb8dc9e7ad79a298b960fe3e61028f5
Filip Askviken <filip@askviken.se>
2012-05-16 23:45:29 +0200

    RID:3.2 OPEN - task 17: Implement libris search of ISBN http://github.com/oscarbr/Brainiacs-startup/issues/issue/17

--- Muffins/src/itBrainiacs/muffins/AddBookActivity.java
+++ Muffins/src/itBrainiacs/muffins/AddBookActivity.java
@@ -1,4 +1,3 @@
-
 package itBrainiacs.muffins;
 
 import android.app.Activity;
@@ -28,27 +27,23 @@ public class AddBookActivity extends Activity implements OnClickListener {
 	private EditText priceET;
 	private EditText commentET;
 	private Button takePictureButton;
+	private Button searchBookButton;
 	private Button addButton;
-	
+
 	DataBook newBook;
 	/*
 	 * To be implemented. To allow the poster of the book to edit/delete
+	 * 
+	 * private String name =""; private String email = ""; private String phone
+	 * = ""; private String password = "";
+	 */
 
-	private String name ="";
-	private String email = "";
-	private String phone = "";
-	private String password = "";
-	*/
-
-
-	
-	/* The code for activity request camera. To be implemented
-	 * private final static int CAMERA_CATCHING_CODE = 100; 
-	 * private final static int MEDIA_TYPE_IMAGE = 1;
-	 * private Uri imageUri;
+	/*
+	 * The code for activity request camera. To be implemented private final
+	 * static int CAMERA_CATCHING_CODE = 100; private final static int
+	 * MEDIA_TYPE_IMAGE = 1; private Uri imageUri;
 	 */
 	private final static int USER_DETAIL_REQUEST = 1;
-	
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -62,26 +57,45 @@ public class AddBookActivity extends Activity implements OnClickListener {
 		courseET = (EditText) findViewById(R.id.addBookCourseET);
 		priceET = (EditText) findViewById(R.id.addBookPriceET);
 		commentET = (EditText) findViewById(R.id.addBookCommentET);
-		
+
 		/*
-		 * Picturebutton to be implemented. 
-		takePictureButton = (Button) findViewById(R.id.addBookTakePictureButton);
-		*/
+		 * Picturebutton to be implemented. takePictureButton = (Button)
+		 * findViewById(R.id.addBookTakePictureButton);
+		 */
+		searchBookButton = (Button) findViewById(R.id.searchBookButton);
 		addButton = (Button) findViewById(R.id.addBookAddButton);
 
-		/* Setting the 'add' button to execute onClick() when pressed */
+		/* Setting buttons to execute onClick() when pressed */
+		searchBookButton.setOnClickListener(this);
 		addButton.setOnClickListener(this);
 	}
-	
-	/* Method from the interface OnClickListener. Called when the any button that has this class as a listener is pressed*/
+
+	/*
+	 * Method from the interface OnClickListener. Called when the any button
+	 * that has this class as a listener is pressed
+	 */
 	public void onClick(View view) {
-		if (view.getId() == R.id.addBookTakePictureButton) {
-			
-			//takePicture();
-			
+		if (view.getId() == R.id.searchBookButton) {
+			searchIsbn();
 		} else if (view.getId() == R.id.addBookAddButton) {
-				addBook();
+			addBook();
+		} else if (view.getId() == R.id.addBookTakePictureButton) {
+			// takePicture();
 		}
+	}
+
+	/**
+	 * Method executed when search Libris button is pressed, fills proper Edit
+	 * text fields with found data
+	 * 
+	 * @return void
+	 */
+	private void searchIsbn() {
+		String isbn = isbnET.getText().toString();
+		DataBook librisResultBook = DataBookFactory.searchLibris(isbn);
+
+		authorET.setText(librisResultBook.getAuthor());
+		titleET.setText(librisResultBook.getTitle());
 	}
 
 	/**
@@ -103,16 +117,15 @@ public class AddBookActivity extends Activity implements OnClickListener {
 		/* Validating the input provided by the user */
 		boolean validInput = checkInput();
 		if (validInput) {
-			
+
 			Intent userDataIntent = new Intent();
-			userDataIntent.setClass(AddBookActivity.this, SettingsActivity.class);
+			userDataIntent.setClass(AddBookActivity.this,
+					SettingsActivity.class);
 			userDataIntent.addCategory("USER_DETAIL_CHECK");
 			startActivityForResult(userDataIntent, USER_DETAIL_REQUEST);
 
-
 		}
 	}
-	
 
 	/**
 	 * Validates the user-input while attempting to add a new book, before
@@ -131,7 +144,7 @@ public class AddBookActivity extends Activity implements OnClickListener {
 	private boolean checkInput() {
 		String badInput = "Please correct the following input before proceeding:\n\n";
 		boolean validInput = true;
-	
+
 		if (newBook.getAuthor() == null || newBook.getAuthor().length() < 4) {
 			badInput = badInput
 					+ "--Author name must be greater than 4 characters.\n";
@@ -144,15 +157,16 @@ public class AddBookActivity extends Activity implements OnClickListener {
 		}
 		String isbn = newBook.getIsbn();
 		if (isbn != null) {
-			if ((isbn.length() != 0
-					&& ((isbn.length() != 10) && (isbn.length() != 13) || !onlyDigits(isbn)))) {
+			if ((isbn.length() != 0 && ((isbn.length() != 10)
+					&& (isbn.length() != 13) || !onlyDigits(isbn)))) {
 				badInput = badInput
 						+ "--The ISBN-number must be 10 or 13 digits long.\n";
 				validInput = false;
 			}
 		}
 		String price = newBook.getPrice();
-		if (price == null || (!(price.length() > 0 && price.length() < 5) || !onlyDigits(price))) {
+		if (price == null
+				|| (!(price.length() > 0 && price.length() < 5) || !onlyDigits(price))) {
 			badInput = badInput
 					+ "--Please specify a price within the range of 0-10000 SEK.\n";
 			validInput = false;
@@ -176,72 +190,71 @@ public class AddBookActivity extends Activity implements OnClickListener {
 
 		return validInput;
 	}
-	
+
 	/**
 	 * Checks to see that the string contains only digits
+	 * 
 	 * @param s
 	 * @return true if no non-digit characters were found
 	 */
-	private boolean onlyDigits(String s){
-		for (int i=0; i < s.length(); i++) {
+	private boolean onlyDigits(String s) {
+		for (int i = 0; i < s.length(); i++) {
 			if (!Character.isDigit(s.charAt(i)))
 				return false;
 		}
 		return true;
 	}
-	
-
 
 	/*
 	 * Method called when the 'Take Picture' button has been pressed and the
 	 * user wants to add a photo of their book
+	 * 
+	 * private void takePicture() {
+	 * 
+	 * Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	 * 
+	 * // http://developer.android.com/guide/topics/media/camera.html#intents //
+	 * imageUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); }
+	 */
 
-	private void takePicture() {
-
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-		// http://developer.android.com/guide/topics/media/camera.html#intents
-		// imageUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-	}
-
-	*/
-	
-	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == USER_DETAIL_REQUEST) {
 			if (resultCode == RESULT_OK) {
-								
-				String communicationResults = ServerCommunicator.addBook(newBook);
+
+				String communicationResults = ServerCommunicator
+						.addBook(newBook);
 				if (communicationResults.equals("1"))
 					Toast.makeText(getApplicationContext(), "Book uploaded",
-						Toast.LENGTH_LONG).show();
-				else 
-					Toast.makeText(getApplicationContext(), "Book failed to upload",
 							Toast.LENGTH_LONG).show();
+				else
+					Toast.makeText(getApplicationContext(),
+							"Book failed to upload", Toast.LENGTH_LONG).show();
 
-			}
-			else {
-				Toast.makeText(getApplicationContext(), "Please insert the required user details" , Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(getApplicationContext(),
+						"Please insert the required user details",
+						Toast.LENGTH_SHORT).show();
 				Intent goToSettingsIntent = new Intent();
-				goToSettingsIntent.setClass(AddBookActivity.this, SettingsActivity.class);
+				goToSettingsIntent.setClass(AddBookActivity.this,
+						SettingsActivity.class);
 				startActivity(goToSettingsIntent);
 			}
 		}
-		
-		/* The Result comes from a camera activity 
-		if (requestCode == CAMERA_CATCHING_CODE) 
-			// Image has been captured and saved to a path specified in the intent
-			if (resultCode == RESULT_OK) {
-				data.getData();
-				// Image has not been captured due to user cancelling process
-	        } else if (resultCode == RESULT_CANCELED) {
-	        	Toast.makeText(getApplicationContext(), "Oh, snap, you cancelled, why did you do that?", Toast.LENGTH_SHORT).show();
-	        // Something has gone terribly wrong and the application failed to capture image.
-	        } else {
-	        	Toast.makeText(getApplicationContext(), "Ehh, something went wrong and no image was captured", Toast.LENGTH_SHORT);
-	        } */
-		}
-	
+
+		/*
+		 * The Result comes from a camera activity if (requestCode ==
+		 * CAMERA_CATCHING_CODE) // Image has been captured and saved to a path
+		 * specified in the intent if (resultCode == RESULT_OK) {
+		 * data.getData(); // Image has not been captured due to user cancelling
+		 * process } else if (resultCode == RESULT_CANCELED) {
+		 * Toast.makeText(getApplicationContext(),
+		 * "Oh, snap, you cancelled, why did you do that?",
+		 * Toast.LENGTH_SHORT).show(); // Something has gone terribly wrong and
+		 * the application failed to capture image. } else {
+		 * Toast.makeText(getApplicationContext(),
+		 * "Ehh, something went wrong and no image was captured",
+		 * Toast.LENGTH_SHORT); }
+		 */
+	}
+
 }
-
-
