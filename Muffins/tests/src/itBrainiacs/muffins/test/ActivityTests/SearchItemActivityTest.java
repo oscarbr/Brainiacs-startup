bf4ea759b06f8bfbda1eb419a4fd387167f1fa85
Rikard Andersson <mail@rikardandersson.se>
2012-05-16 16:18:56 +0200

    OPEN - task 16: Tests missing for some methods http://github.com/oscarbr/Brainiacs-startup/issues/issue/16 Added tests for save- and delete-methods in SettingsActivity.

---
+++ Muffins/tests/src/itBrainiacs/muffins/test/ActivityTests/SearchItemActivityTest.java
@@ -0,0 +1,26 @@
+package itBrainiacs.muffins.test.ActivityTests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import itBrainiacs.muffins.SearchItemActivity;
+
+public class SearchItemActivityTest extends ActivityInstrumentationTestCase2<SearchItemActivity> {
+
+	SearchItemActivity searchItemActivity;
+	
+	public SearchItemActivityTest() {
+		super("itBrainiacs.muffins", SearchItemActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		setActivityInitialTouchMode(false);
+		
+		searchItemActivity = getActivity();
+	}
+	
+	public void testPreConditions() {
+		assertTrue(searchItemActivity != null);
+	}
+}
--- Muffins/tests/src/itBrainiacs/muffins/test/ActivityTests/SettingsActivityTest.java
+++ Muffins/tests/src/itBrainiacs/muffins/test/ActivityTests/SettingsActivityTest.java
@@ -1,11 +1,16 @@
 package itBrainiacs.muffins.test.ActivityTests;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 import itBrainiacs.muffins.SettingsActivity;
 
 public class SettingsActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity> {
 
 	SettingsActivity settingsActivity;
+	EditText name;
+	EditText email;
+	EditText phone;
+	EditText password;
 	
 	public SettingsActivityTest() {
 		super("itBrainiacs.muffins", SettingsActivity.class);
@@ -23,4 +28,57 @@ public class SettingsActivityTest extends ActivityInstrumentationTestCase2<Setti
 		assertTrue(settingsActivity != null);
 	}
 	
+	public void testSaveSettings() {
+		
+		String testName = "TestName";
+		String testEmail = "TestEmail";
+		String testPhone = "1";
+		String testPassword = "TestPassword";
+		
+		setupEditTexts();
+		
+		name.setText(testName);
+		email.setText(testEmail);
+		phone.setText(testPhone);
+		password.setText(testPassword);
+		
+		settingsActivity.onClick(settingsActivity.findViewById(itBrainiacs.muffins.R.id.settingsSaveButton));
+		settingsActivity.finish();
+		settingsActivity = getActivity();
+		
+		setupEditTexts();
+		
+		assertEquals(name.getText().toString(), testName);
+		assertEquals(email.getText().toString(), testEmail);
+		assertEquals(phone.getText().toString(), testPhone);
+		assertEquals(password.getText().toString(), testPassword);
+	}
+	
+	public void testDeleteSettings() {
+		setupEditTexts();
+		settingsActivity.onClick(settingsActivity.findViewById(itBrainiacs.muffins.R.id.settingsDeleteButton));
+		
+		assertEquals(name.getText().toString(), "");
+		assertEquals(email.getText().toString(), "");
+		assertEquals(phone.getText().toString(), "");
+		assertEquals(password.getText().toString(), "");
+	}
+	
+	private void setupEditTexts() {
+		
+		name = (EditText) settingsActivity.findViewById(itBrainiacs.muffins.R.id.settingsNameET);
+		email = (EditText) settingsActivity.findViewById(itBrainiacs.muffins.R.id.settingsEmailET);
+		phone = (EditText) settingsActivity.findViewById(itBrainiacs.muffins.R.id.settingsPhoneET);
+		password = (EditText) settingsActivity.findViewById(itBrainiacs.muffins.R.id.settingsPasswordET);
+		
+		String testName = "TestName";
+		String testEmail = "TestEmail";
+		String testPhone = "1";
+		String testPassword = "TestPassword";
+				
+		name.setText(testName);
+		email.setText(testEmail);
+		phone.setText(testPhone);
+		password.setText(testPassword);
+	}
 }
